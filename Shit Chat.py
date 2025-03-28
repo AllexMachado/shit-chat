c9b705f759a7678b862908014871e2cc932639de
Allex Machado <itsnotallexmachado@gmail.com>
2025-03-28 12:56:29 -0300

    Update Shit Chat 3.py

--- Shit Chat.py
+++ Shit Chat.py
@@ -1,42 +1,36 @@
-name = input("Hi! What was your name again? Since it's been so long we had a shit chat ")
-
-havingkids = ['yes','Yes!','I do','yup','duh','yeah']
-nothavingkids = ['nah','tf no','NO!?','ew, no','no?','no','No','NO']
+havingkids = ['yes', 'Yes!', 'I do', 'yup', 'duh', 'yeah']
+nothavingkids = ['nah', 'tf no', 'NO!?', 'ew, no', 'no?', 'no', 'No', 'NO']
 creator = ['Allex']
 
-def check_name(name):
-    if name in creator:
-        print("What...no you... you can't be me...")
-    else:
-        check_age(age)
+def get_valid_name():
+    while True:
+        name = input("Hi! What was your name again? Since it's been so long we had a chit-chat ")
+        if name in creator:
+            print("What... no you... you can't be me... Answer YOUR name >:(")
+        else:
+            return name
 
-age = input("Damn, how old are you now? ")
+def get_valid_age():
+    while True:
+        age = input("Damn, how old are you now? ")
+        if age.isdigit():
+            return int(age)
+        else:
+            print("Tf, that's not an age, tell me your REAL age man.")
 
-def check_age(age):
-    if age.isdigit():
-        age = int(age)
-        print("Damn, you aged...")
-        check_ageness(age)
-    else:
-        print("Tf... that's not an age.")
-        return
+def check_kids():
+    while True:
+        kids = input("Dude... do you... like... have kids now? " if age >= 35 else "Ik is too soon, but do you plan on having kids one day? ")
+        if kids in havingkids:
+            print("DAMN, so you DID age like a parent...")
+            break
+        elif kids in nothavingkids:
+            print("WOW okay...")
+            break
+        else:
+            print("Dude, be serious.")
 
-def check_ageness(age):
-    if age >= 35:
-        kids = input("Dude...do you...like...have kids now? ")
-        check_kids(kids)  
-    elif age <= 35:
-        print("OOOOh, you're too young you little silly billy :3...")
-        kids = input("But do you plan on having kids soon? ")
-        check_kids(kids)  
-
-def check_kids(kids):
-    if kids in havingkids:
-        print("GESSS, so you DID age like a parent...")
-    elif kids in nothavingkids:
-        print('WOW okay...')
-    else:
-        print("I guess you didn't answer properly.")
-
-check_name(name)
+name = get_valid_name()
+age = get_valid_age()
+check_kids()
 
